c43dbb00317d3be69b4e71d246a960e881388053
AVISHKA SHAVINDA <134716496+avishka33@users.noreply.github.com>
2025-08-05 13:27:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,12 +22,7 @@ global.fake = {
 	listfakedocs: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.openxmlformats-officedocument.presentationml.presentation','application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/pdf'],
 }
 
-global.my = {
-	yt: 'https://youtube.com/Avi_Nethunter',
-	gh: 'https://github.com/avishka33',
-	gc: 'https://whatsapp.com/channel/0029VaGisHDFHWq5jpuEmC2t',
-	ch: 'https://whatsapp.com/channel/0029VaGisHDFHWq5jpuEmC2t',
-}
+
 
 global.limit = {
 	free: 20,
@@ -55,13 +50,7 @@ global.mess = {
 	done: 'Done'
 }
 
-global.APIs = {
-	hitori: 'https://api.hitori.pw',
-}
-global.APIKeys = {
-	'https://api.hitori.pw': 'htrkey-77eb83c0eeb39d40',
-	geminiApikey: ['AIzaSyD0lkGz6ZhKi_MHSSmJcCX3wXoDZhELPaQ','AIzaSyDnBPd_EhBfr73NssnThVQZYiKZVhGZewU','AIzaSyA94OZD-0V4quRbzPb2j75AuzSblPHE75M','AIzaSyB5aTYbUg2VQ0oXr5hdJPN8AyLJcmM84-A','AIzaSyB1xYZ2YImnBdi2Bh-If_8lj6rvSkabqlA']
-}
+
 
 
 global.badWords = ['පකයා','හුත්තො','පොන්නයා','වේසි']
@@ -76,4 +65,5 @@ fs.watchFile(file, () => {
 	delete require.cache[file]
 	require(file)
 });
+
 
