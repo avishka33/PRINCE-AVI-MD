d4b63bc305acdb9206c6cbf042783bd282beef5c
AVISHKA SHAVINDA <134716496+avishka33@users.noreply.github.com>
2025-08-12 09:37:28 -0700

    Create pastpaper.js

---
+++ AMedia/school/pastpaper.js
@@ -0,0 +1,11 @@
+module.exports = async function
+ppCommands(m.command) {
+
+switch (m.command){
+  case 'test1':
+    await Avishka esndMessage(m.chat, {
+      text: 'this message is testing paper.js'},
+    { quoted: m });
+  break
+ }
+}
